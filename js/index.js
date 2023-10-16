9c292f2d74fa55ee9322622e715fd44f4598feea
Saqlain <saqlainsj12@gmail.com>
2023-10-16 09:53:54 +0500

    UI Complete

--- js/index.js
+++ js/index.js
@@ -5,3 +5,28 @@ menu.addEventListener("click", function () {
   navUl.classList.toggle("show-ul");
   menu.classList.toggle("active");
 });
+
+// hover freelancer work
+
+const freelanceWork = document.querySelectorAll(".freelancer-work");
+
+freelanceWork.forEach((work) => {
+  work.addEventListener("mouseenter", function () {
+    const imgContainer = work.querySelector(".img-container");
+    imgContainer.classList.add("bg-hover");
+  });
+
+  work.addEventListener("mouseleave", function () {
+    const imgContainer = work.querySelector(".img-container");
+    imgContainer.classList.remove("bg-hover");
+    imgContainer.classList.remove("bg-click");
+  });
+
+  work.addEventListener("mousedown", function () {
+    const imgContainer = work.querySelector(".img-container");
+    imgContainer.classList.add("bg-click");
+    console.log(imgContainer);
+  });
+});
+
+// quote generator
